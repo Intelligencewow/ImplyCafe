96bfab78e358281c7348de3a3abf6e5db51d6fb0
Alisson Henrique <alissonhenrique1322@gmail.com>
2024-10-03 11:49:53 -0300

    Update

--- app/src/main/java/com/example/implycafe/MainActivity.java
+++ app/src/main/java/com/example/implycafe/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.implycafe;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ProductAdapter.onItemClickListener{
+    private RecyclerView recyclerView;
+    private ProductAdapter productAdapter;
+    private List<Product> productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        productList = new ArrayList<>();
+        productList.add(new Product("Coca cola", "ela tá gelada", "10"));
+        productList.add(new Product("Pepsi", "Pepsi geladinha", "15"));
+        productList.add(new Product("Fanta", "ela tá gelada", "25"));
+
+        productAdapter = new ProductAdapter(this, productList);
+        recyclerView.setAdapter(productAdapter);
+
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Product product = productList.get(position);
+        Toast.makeText(this, "Clicked: " + product.getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemLongClick(int position) {
+        Product product = productList.get(position);
+        Toast.makeText(this, "Long clicked: " + product.getName(), Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/implycafe/ProductAdapter.java
@@ -0,0 +1,77 @@
+package com.example.implycafe;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> implements View.OnClickListener {
+    private List<Product> productList;
+    private Context context;
+    private onItemClickListener onItemClickListener;
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public interface onItemClickListener{
+        void onItemClick(int position);
+        void onItemLongClick(int position);
+    }
+    public ProductAdapter(Context context, List<Product> productList) {
+        this.productList = productList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_product, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+        holder.productName.setText(product.getName());
+        holder.productDescription.setText(product.getDescription());
+        holder.productPrice.setText(product.getPrice());
+
+        holder.itemView.setOnClickListener(v -> {
+            if(onItemClickListener != null){
+                onItemClickListener.onItemClick(position);
+            }
+        });
+
+        holder.itemView.setOnClickListener(v -> {
+            if(onItemClickListener != null){
+                onItemClickListener.onItemLongClick(position);
+            }
+        });
+
+    }
+
+    public int getItemCount(){
+        return productList.size();
+    }
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder{
+        public TextView productName;
+        public TextView productDescription;
+        public TextView productPrice;
+
+        public ProductViewHolder(View itemView){
+            super(itemView);
+            productName = itemView.findViewById(R.id.productName);
+            productDescription = itemView.findViewById(R.id.productDescription);
+            productPrice = itemView.findViewById(R.id.productPrice);
+
+        }
+    }
+}
